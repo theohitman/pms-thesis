f3ccfbdb5b54bc368abbaed9bd9313dfb7cf8abf
Theodoros Diplas <theodiplas@gmail.com>
2021-12-12 10:28:49 +0200

    add various wifi sketches

--- ESP8266/Blink_LED.cpp
+++ ESP8266/Blink_LED.cpp
@@ -6,8 +6,12 @@
  */
 #include "Arduino.h"
 
+// Set LED_BUILTIN if it is not defined by Arduino framework
+// #define LED_BUILTIN 13
+
 void setup()
 {
+  Serial.begin(9600);
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
@@ -16,12 +20,14 @@ void loop()
 {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("LED is ON!!!");
 
   // wait for a second
   delay(1000);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("LED is OFF!!!");
 
    // wait for a second
   delay(1000);
